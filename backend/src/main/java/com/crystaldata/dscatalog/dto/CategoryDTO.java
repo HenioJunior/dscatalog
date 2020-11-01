20191c863b44601751eaebbed78f054410e84132
Henio Junior <henioajunior@gmail.com>
2020-11-01 18:41:52 -0300

    DTO

---
+++ backend/src/main/java/com/crystaldata/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,41 @@
+package com.crystaldata.dscatalog.dto;
+
+import com.crystaldata.dscatalog.entities.Category;
+
+import java.io.Serializable;
+
+public class CategoryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO (){}
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- backend/src/main/java/com/crystaldata/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/crystaldata/dscatalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.crystaldata.dscatalog.services;
 
+import com.crystaldata.dscatalog.dto.CategoryDTO;
 import com.crystaldata.dscatalog.entities.Category;
 import com.crystaldata.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +18,8 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 }
