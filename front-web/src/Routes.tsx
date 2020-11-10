396f2e34cbbd9ef15c52f312b561d19b69fb0dd4
Henio Junior <henioajunior@gmail.com>
2020-11-10 16:51:21 -0300

    Building authentication structure

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -5,7 +5,7 @@ import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
-
+import Auth from './pages/Auth';
 
 const Routes = () => (
     <BrowserRouter>
@@ -15,17 +15,18 @@ const Routes = () => (
                 <Home />
             </Route>
             <Route path="/products" exact>
-            <Catalog />
+                <Catalog />
             </Route>
             <Route path="/products/:productId">
-            <ProductDetails />
+                <ProductDetails />
             </Route>
-            <Route>
-                
+            <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+            <Route path="/admin/auth">
+                <Auth />
             </Route>
             <Redirect from="/admin" to="/admin/products" exact />
             <Route path="/admin">
-            <Admin />
+                <Admin />
             </Route>
         </Switch>
     </BrowserRouter>
