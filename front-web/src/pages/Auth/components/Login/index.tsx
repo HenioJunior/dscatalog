305972196dd4184feafaa837fb9b0d3c49141ff5
Henio Junior <henioajunior@gmail.com>
2020-11-16 10:41:42 -0300

    Display alert when invalid login

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,5 +1,5 @@
 import ButtonIcon from 'core/components/Buttonicon';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
@@ -13,28 +13,38 @@ type FormData = {
 
 const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
+  const [hasError, setHasError] = useState(false);
 
   const onSubmit = (data: FormData) => {
     console.log(data);
-    makeLogin(data);
+    makeLogin(data)
+    .then(response => {setHasError(false);
+    })
+    .catch(() => {setHasError(true);
+    })
   }
 
   return (
     <AuthCard title="Login">
+      {hasError && (
+      <div className="alert alert-danger mt5">
+        Usuário ou senha inválidos!
+      </div>
+      )}
       <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
       <input 
       type="email"
       className="form-control input-base margin-bottom-30"
       placeholder="Email"
       name="username"
-      ref={register}
+      ref={register({ required: true })}
       />
        <input 
       type="password"
       className="form-control input-base"
       placeholder="Senha"
       name="password"
-      ref={register}
+      ref={register({ required: true , minLength: 5 })}
       />
       <Link to="/admin/auth/recover" className="login-link-recover">
       Esqueci a senha?
