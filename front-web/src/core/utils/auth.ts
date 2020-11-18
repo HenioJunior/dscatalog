e5a9a33e9ab0d1c350408e2af6e2ae77a8069c7e
Henio Junior <henioajunior@gmail.com>
2020-11-18 15:01:50 -0300

    Add allowed roles on private route

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -11,7 +11,7 @@ type LoginResponse = {
   userId: number;
   }
 
-  type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+  export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
   
   type AccessToken = {
     exp: number;
@@ -47,4 +47,14 @@ type LoginResponse = {
     const sessionData = getSessionData();
     
     return sessionData.access_token && isTokenValid
+  }
+
+  export const isAllowedByRole = (routeRoles: Role[] = []) => {
+    if(routeRoles.length === 0){
+      return true;
+    }
+    
+    const { authorities } = getAccessTokenDecoded();
+    
+    return routeRoles.some(role => authorities.includes(role));
   }
