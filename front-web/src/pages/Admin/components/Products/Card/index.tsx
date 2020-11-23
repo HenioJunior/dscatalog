ba838fb4e24f74a24ea88b9c42c525dfb93d9468
Henio Junior <henioajunior@gmail.com>
2020-11-23 20:58:13 -0300

    Creating product list layout 2

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,9 +1,10 @@
+import ProductPrice from 'core/components/ProductPrice';
 import React from 'react';
 import './styles.scss';
 
 const Card = () => {
   return (
-    <div className="card-base product-card-admin">
+  <div className="card-base product-card-admin">
     <div className="row">
       <div className="col-2 text-center border-right py-3">
         <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
@@ -12,15 +13,28 @@ const Card = () => {
         />
       </div>
       <div className="col-7 py-3">
-        <h1 className="product-card-name-admin">
+      <h3 className="product-card-name-admin">
           Computador i7
-        </h1>
+        </h3>
+        <ProductPrice price={40.5} />
       </div>
-      <div className="col3 py-3">
-        <h3>Ações</h3>
+      <div className="col-3 pt-3 pr-5">
+        <button
+        type="button"
+        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit"
+        >
+          EDITAR
+        </button>
+        <button
+        type="button"
+        className="btn btn-outline-danger btn-block border-radius-10 btn-delete"
+        >
+        EXCLUIR
+        </button>
       </div>
-    </div>
-    </div>
+  
+    </div>  
+  </div>
   )
 }
 
