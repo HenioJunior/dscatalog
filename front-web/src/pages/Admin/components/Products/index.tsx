4c52c63be911a028277fdcbed06797112fe3274b
Henio Junior <henioajunior@gmail.com>
2020-11-24 11:57:14 -0300

    Creating edit Prod page 1/3

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -7,18 +7,15 @@ const Products = () => {
     return (
         <div>
             <Switch>
-            <Route path="/admin/products" exact>
+                <Route path="/admin/products" exact>
                     <List />
                 </Route>
-                <Route path="/admin/products/create">
-                    <Form />
-                </Route>
                 <Route path="/admin/products/:productId">
-                    <h1>Editar um produto</h1>
+                    <Form />
                 </Route>
             </Switch>
         </div>
     );
-    }
+}
 
-    export default Products;
+export default Products;
