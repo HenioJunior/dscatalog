1b8c245ed638bd2055ab709e51e8cb8473fc5d49
Henio Junior <henioajunior@gmail.com>
2021-01-02 14:46:38 -0300

    Fix: Postgres compatibility

--- backend/src/main/java/com/crystaldata/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/crystaldata/dscatalog/repositories/ProductRepository.java
@@ -1,12 +1,22 @@
 package com.crystaldata.dscatalog.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.crystaldata.dscatalog.entities.Category;
 import com.crystaldata.dscatalog.entities.Product;
 
 //Camada de acesso a dados
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+			+ "(:name = '' OR LOWER (obj.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
+	Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
