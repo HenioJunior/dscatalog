60ff1b14bf75f36c50823ae900118f069457ac94
Henio Junior <henioajunior@gmail.com>
2021-01-25 10:28:41 -0300

    Find Should Return All Products When Name Exists Is Empty - TEST

--- backend/src/test/java/com/crystaldata/dscatalog/tests/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/crystaldata/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -26,6 +26,7 @@ public class ProductRepositoryTests {
     private Long nonExistingId;
     private Long countTotalProducts;
     private Long countPCGamerProducts;
+    private PageRequest pageRequest;
     
     @BeforeEach
     void setUp() throws Exception {
@@ -33,25 +34,45 @@ public class ProductRepositoryTests {
     	nonExistingId = 1000L;
     	countTotalProducts = 25L;
     	countPCGamerProducts = 21L;
+    	pageRequest = PageRequest.of(0, 10);
     }
     
     @Test
-    public void findShouldReturnProductsWhenNameExists() {
+    public void findShouldReturnAllProductsWhenNameExistsIsEmpty() {
     	
-    	String name = "PC Gamer";
-    	PageRequest pageRequest = PageRequest.of(0, 10);
+    	String name = "";
+    	    	
+    	Page<Product> result = repository.find(null, name, pageRequest);
     	
+    	Assertions.assertFalse(result.isEmpty());
+    	Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+    
+    @Test
+    public void findShouldReturnProductsWhenNameExistsIgnoringCase() {
+    	
+    	String name = "pc gAMeR";
+    	    	
     	Page<Product> result = repository.find(null, name, pageRequest);
     	
     	Assertions.assertFalse(result.isEmpty());
     	Assertions.assertEquals(countPCGamerProducts, result.getTotalElements());
-    	
     }
-    
-    
-    
+       
+    @Test
+    public void findShouldReturnProductsWhenNameExists() {
+    	
+    	String name = "PC Gamer";
+    	    	
+    	Page<Product> result = repository.find(null, name, pageRequest);
+    	
+    	Assertions.assertFalse(result.isEmpty());
+    	Assertions.assertEquals(countPCGamerProducts, result.getTotalElements());
+    }
+       
     @Test
     public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+    	
     	Product product = ProductFactory.createProduct();
     	product.setId(null);
     	
@@ -62,7 +83,6 @@ public class ProductRepositoryTests {
     	Assertions.assertEquals(countTotalProducts + 1L, product.getId());
     	Assertions.assertTrue(result.isPresent());
     	Assertions.assertSame(result.get(), product);
-    	
     }
 
     @Test
