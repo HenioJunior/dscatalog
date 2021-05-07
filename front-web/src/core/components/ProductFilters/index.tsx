37df0100a92481b43ef36e0e8dedae9c557e8264
Henio Junior <henioajunior@gmail.com>
2021-05-07 14:36:48 -0300

    Fix Product filters

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -5,22 +5,25 @@ import './styles.scss';
 import { makeRequest } from 'core/utils/request';
 import { Category } from 'core/types/Product';
 
-export type FilterForm = {
-  name?: string;
-  categoryId?: number;
-}
-
 type Props = {
-  onSearch: (filter: FilterForm) => void;
+  name?: string;
+  category?: Category;
+  handleChangeName: (name: string) => void;
+  handleChangeCategory: (category: Category) => void;
+  clearFilters: () => void;
 }
 
-const ProductFilters = ({ onSearch }: Props) => {
+const ProductFilters = ({
+  name,
+  handleChangeName,
+  handleChangeCategory,
+  clearFilters,
+  category
+  }: Props) => {
 
   const [isLoadingCategories, setIsLoadingCategories] = useState(false);
   const [categories, setCategories] = useState<Category[]>([]);
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState<Category>();
-
+  
   useEffect(() => {
     setIsLoadingCategories(true);
     makeRequest({ url: '/categories' })
@@ -28,31 +31,12 @@ const ProductFilters = ({ onSearch }: Props) => {
       .finally(() => setIsLoadingCategories(false));
   }, []);
 
-  const handleChangeName = (name: string) => {
-    setName(name);
-
-    onSearch({ name, categoryId: category?.id });
-  }
-
-  const handleChangeCategory = (category: Category) => {
-    setCategory(category);
-
-    onSearch({ name, categoryId: category?.id });
-  }
-
-  const clearFilters = () => {
-    setCategory(undefined);
-    setName('');
-
-    onSearch({ name: '', categoryId: undefined });
-  }
-
   return (
     <div className="card-base product-filters-container">
       <div className="input-search">
         <input
           type="text"
-          value={ name }
+          value={name}
           className="form-control"
           placeholder="Pesquisar produto"
           onChange={event => handleChangeName(event.target.value)}
